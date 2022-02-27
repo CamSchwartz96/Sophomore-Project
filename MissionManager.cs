282e60850f1059ae55805ebcabf31f93824fab07
Cam Schwartz <cameronschwartz@oakland.edu>
2022-02-27 15:49:04 -0500

    completed Mission Manager

--- MissionManager.cs
+++ MissionManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class MissionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MissionManager instance;
+
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<Mission> missionList = new List<Mission>();
 }
