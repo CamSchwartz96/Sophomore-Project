5dfaf2f3113998f338f03d2a1377c6d11d85c085
Cam Schwartz <cameronschwartz@oakland.edu>
2022-02-26 21:27:33 -0500

    small update

--- Mission.cs
+++ Mission.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mission : MonoBehaviour
+public class Mission
 {
-    
+    public int missionId;
+    [Space]
+    public bool active;
+    public bool missionComplete;
+    [Space]
+    public bool restartOnNextBall;
 }
