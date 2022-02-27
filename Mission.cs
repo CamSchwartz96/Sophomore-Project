1fc2a6523d2afe1d07398e54a89f2ad1d1ca09b0
Cam Schwartz <cameronschwartz@oakland.edu>
2022-02-27 15:27:56 -0500

    completed mission

--- Mission.cs
+++ Mission.cs
@@ -13,5 +13,51 @@ public class Mission
     public bool stopOnBallEnd;
     public bool resetOnComplete;
     [Space]
-    public float timeOnComplete;
+    public float timeToComplete;
+    [Space]
+    public int score;
+    public int amountToComplete;
+    [HideInInspector]public int currentAmount;
+
+    public void ResetMission()
+    {
+        if(resetOnComplete)
+        {
+            active = false;
+            missionComplete = false;
+            currentAmount = 0;
+        }
+    }
+
+    public void DeactivateMission()
+    {
+        active = false;
+        currentAmount = 0;
+    }
+
+    public void UpdateMission()
+    {
+        if(active && !missionComplete)
+        {
+            currentAmount++;
+
+            //check if mission is complete
+        }
+    }
+
+    void CheckMissionComplete()
+    {
+        if(currentAmount >= amountToComplete)
+        {
+            missionComplete = true;
+            active = false;
+
+            if(timeToComplete > 0)
+            {
+                //STOP TIMER > Mission Manager
+            }
+
+            ResetMission();
+        }
+    }
 }
