ee42d3892124250397d451203484e983bda828b7
Cam Schwartz <cameronschwartz@oakland.edu>
2022-03-06 09:52:10 -0500

    more work being done

--- MissionManager.cs
+++ MissionManager.cs
@@ -6,10 +6,33 @@ public class MissionManager : MonoBehaviour
 {
     public static MissionManager instance;
 
+    bool timeBasedMissionActive;
+
     void Awake()
     {
         instance = this;
     }
 
     public List<Mission> missionList = new List<Mission>();
+
+    public void StartMission(int ID)
+    {
+        foreach (Mission mission in missionList)
+        {
+            if (mission.missionID == ID)
+            {
+                if(!mission.missionComplete && !mission.active && mission.timeToComplete>0 && !timeBasedMissionActive)
+                {
+                    mission.active = true;
+                    timeBasedMissionActive = true;
+                }
+
+                else if(!mission.missionComplete && !mission.active && mission.timeToComplete <= 0)
+                {
+                    mission.active = true;
+                }
+            }
+
+        }
+    }
 }
